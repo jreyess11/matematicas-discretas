50a2d2900e46d5dfc5703b362edc383cfb4dfc2f
jreyess11 <josegabrielreyessolanojr@gmail.com>
2022-08-04 20:03:39 -0500

    Update descNum.py

--- descNum.py
+++ descNum.py
@@ -5,13 +5,12 @@ import msvcrt
 def desc(n):
     primos = []
     for i in range(2, n + 1):
-        if n % i == 0:
+        while n % i == 0:
             primos.append(i)
             n = n / i
     return primos
 
-
 N = int(input("Ingrese el número a descomponer: "))
-print(desc(N))
+print(*desc(N), sep=" * ")
 
 msvcrt.getch()
